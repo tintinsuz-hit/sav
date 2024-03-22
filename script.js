c6021f997ce438d9eabd602b7a3cadfde01f7b1c
tintinsuz <78978749+tintinsuz-hit@users.noreply.github.com>
2024-03-23 02:08:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+const title = document.querySelector('.title')
+const leaf1 = document.querySelector('.leaf1')
+const leaf2 = document.querySelector('.leaf2')
+const bush2 = document.querySelector('.bush2')
+const mount1 = document.querySelector('.mount1')
+const mount2 = document.querySelector('.mount2')
+
+document.addEventListener('scroll', function() {
+    let value = window.scrollY
+    // console.log(value)
+    title.style.marginTop = value * 1.1 + 'px'
+
+    leaf1.style.marginLeft = -value + 'px'
+    leaf2.style.marginLeft = value + 'px'
+
+    bush2.style.marginBottom = -value + 'px'
+
+    mount1.style.marginBottom = -value * 1.1 + 'px'
+    mount2.style.marginBottom = -value * 1.2 + 'px'
+})
+
+
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+};
+
+var swiper = new Swiper(".gallery-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction:false,
+    }
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+AOS.init({
+  duration:800,
+  delay:400
+});
+
+
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
